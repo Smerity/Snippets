de25fe5ee8f358a2ea00b245af6804e8193970fc
Stephen Merity <smerity@smerity.com>
2014-03-18 02:59:39 -0400

    Chords: Minor code refactor and example of generated file

--- python/chords.py
+++ python/chords.py
@@ -1,36 +1,39 @@
 import numpy as np
 from scipy.io.wavfile import write
 
+# We will have a sampling frequency of 44,100Hz
 RATE = 44100
 NOTES = {
-    'A': 440, 'Bf': 466, 'B': 494, 'C': 523,
-    'Cs': 554, 'D': 587, 'Ds': 587, 'E': 659,
-    'F': 698, 'Fs': 740, 'G': 784, 'Af': 831,
-    'A': 880
+    'Bf': 466, 'B': 494, 'C': 523, 'Cs': 554,
+    'D': 587, 'Ds': 587, 'E': 659, 'F': 698,
+    'Fs': 740, 'G': 784, 'Af': 831, 'A': 880
 }
 
 
-def note(freq, octave=1, length=1, amp=1000):
+def note(freq, octave=1, duration=1, amp=1000):
   if type(freq) == str:
     freq = NOTES[freq]
   freq *= octave
-  X = np.linspace(0, length, length * RATE)
+  X = np.linspace(0, duration, duration * RATE)
   res = np.sin(2 * np.pi * freq * X) * amp
-  print res
   return np.int16(res)
 
-dm = sum([note(x) for x in 'DFA'])
-g = sum([note(x) for x in 'GBD'])
-c = sum([note(x) + note(x, octave=0.5) for x in 'CEG'])
-data = np.concatenate([dm, g, c])
-write('test.wav', 44100, np.int16(data))
+if __name__ == '__main__':
+  # Let's hear what the standard 2-5-1 progression sounds like
+  dm = sum([note(x) for x in 'DFA'])
+  g = sum([note(x) for x in 'GBD'])
+  c = sum([note(x) + note(x, octave=0.5) for x in 'CEG'])
+  data = np.concatenate([dm, g, c])
+  write('2-5-1.wav', 44100, np.int16(data))
 
-"""
-import matplotlib.pyplot as plt
-import seaborn as sns
-for c in 'CEG':
-  sound = note(c, octave=0.5 ** 6)
-  plt.plot(np.arange(len(sound)), sound, label=c)
-plt.legend()
-plt.show()
-"""
+  # Why don't we check out what a CEG chord looks like?
+  import matplotlib.pyplot as plt
+  try:
+    import seaborn as sns
+  except ImportError:
+    pass
+  for c in 'CEG':
+    sound = note(c, octave=0.5 ** 6)
+    plt.plot(np.arange(len(sound)), sound, label=c)
+  plt.legend()
+  plt.show()
