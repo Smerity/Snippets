9aaf775be32987bc25e58aa9cb636a9a7ef4599a
Stephen Merity <smerity@smerity.com>
2019-02-05 20:54:47 -0800

    The ngram command line application :P

--- rust/gist/ngram/src/main.rs
+++ rust/gist/ngram/src/main.rs
@@ -1,10 +1,15 @@
+//extern crate hashbrown;
+
+// Hashbrown seems slower than the standard map for my work
+//use hashbrown::HashMap;
+use std::collections::HashMap;
+
+use std::io::Read;
 use std::env;
 use std::io;
 use std::fs::File;
 use std::io::prelude::*;
 use std::str;
-use std::collections::HashMap;
-use std::collections::VecDeque;
 use std::io::BufReader;
 
 fn main() -> io::Result<()> {
@@ -12,45 +17,58 @@ fn main() -> io::Result<()> {
     //println!("{:?}", args);
     let filename = &args[1];
 
-    let ngram = 5;
+    let ngram = 6;
 
-    let mut ring = VecDeque::new();
-    let mut counts = HashMap::new();
+    //let mut counts = HashMap::new();
+    let mut counts = HashMap::with_capacity(1000);
     let mut tmp : Vec<u8> = vec![0; ngram];
+    let mut cap = 0;
+    let mut seen = 0;
+    let mut dupe = 0;
 
-    // TODO: Byte at a time is crazy slow
+    println!("Reading...");
     let f = File::open(filename)?;
     let reader = BufReader::new(f);
-    for (_, byte) in reader.bytes().enumerate() {
-        ring.push_back(byte.unwrap());
-        if ring.len() >= ngram {
-            if ring.len() > ngram {
-                ring.pop_front();
+    for (idx, byte) in reader.bytes().enumerate() {
+        seen += 1;
+        // We need [_, 0, 1, 2] as the main thread will shift [0, 1, 2, _] and put 3 into the last spot
+        if counts.capacity() != cap {
+            cap = counts.capacity();
+            println!("Capacity: {}", cap);
+        }
+        if idx < ngram - 1 {
+            tmp[idx + 1] = byte.unwrap();
+        } else {
+            // Shift all items left by one
+            for i in 0..ngram-1 {
+                tmp[i] = tmp[i + 1];
             }
-            for (i, v) in ring.iter().enumerate() {
-                tmp[i] = *v;
-            }
+            tmp[ngram-1] = byte.unwrap();
+
             // This is not as concise as an entry based solution
             // An entry(k) requires ownership of k however - even if the key already exists
             // Increasing allocations in this inner loop is disastrous so we check for key existence
             // Discussions: https://internals.rust-lang.org/t/pre-rfc-abandonning-morals-in-the-name-of-performance-the-raw-entry-api/7043/52
             if counts.get_mut(&tmp).is_some() {
                 *counts.get_mut(&tmp).unwrap() += 1;
+                dupe += 1;
             } else {
                 counts.insert(tmp.to_owned(), 1);
             }
         }
     }
 
+    println!("Processing...");
+    println!("Total of {} items", counts.len());
+    println!("Of the {} seen items, {} items are dupes", seen, dupe);
+    println!("Hapax legomenon: {}", counts.values().filter(|&x| *x == 1).count());
     let mut count_vec: Vec<_> = counts.iter().collect();
     count_vec.sort_by(|a, b| b.1.cmp(a.1));
-    for (i, (ngram, count)) in count_vec.iter().enumerate() {
-        if i > 10 {
-            break
-        }
+    for (i, (ngram, count)) in count_vec.iter().take(25).enumerate() {
         let s = str::from_utf8(&ngram).unwrap();
         println!("Rank {}: {:?} with count {}", i, s, count);
     }
+
     // Not sure what this is or why it's needed but io::Result is involved ;)
     Ok(())
 }
