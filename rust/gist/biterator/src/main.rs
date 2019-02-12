2ab4b999856d0f81ea06992d8145dbf80a10bf1c
Stephen Merity <smerity@smerity.com>
2019-02-11 23:29:47 -0800

    Playing with variations on a theme

--- rust/gist/biterator/src/main.rs
+++ rust/gist/biterator/src/main.rs
@@ -22,25 +22,50 @@ fn main() -> io::Result<()> {
 
     let mut seen : u64 = 0;
     let mut sum : u64 = 0;
+    let mut roll : u32 = 0;
+    let mut out = vec![0];
 
     let mut freq : [u64; 256] = [0; 256];
 
     println!("Reading...");
-    for i in 0..40 {
+
+
+
+    for i in 0..10 {
         //seen = 0;
-        for tmp in data.windows(1) {
+        // Avoid indexing for speed : https://llogiq.github.io/2017/06/01/perf-pitfalls.html
+        for tmp in data.iter() {
             seen += 1;
-            sum += tmp[0] as u64;
+            sum += *tmp as u64;
             // The speed is _so_ much faster without freq
-            freq[tmp[0] as usize] += 1;
+            freq[*tmp as usize] += 1;
+
+            // Unsafe isn't much faster as it turns out
+            // The Rustonomicon: The Dark Arts of Advanced and Unsafe Rust Programming
+            // https://doc.rust-lang.org/nightly/nomicon/README.html#the-rustonomicon
+            //unsafe {
+            //    *freq.get_unchecked_mut(*tmp as usize) += 1;
+            //}
+
+            //roll = ((7 * roll + 3 * (roll + (tmp as u32))) << 8) + (tmp as u32);
+            //println!("{}", roll);
+            //roll = (tmp as u32);
 
             /*
-            if counts.contains_key(&tmp) {
-                *counts.get_mut(&tmp).unwrap() += 1;
-            } else {
-                counts.insert(tmp.to_owned(), 1);
+            freq[(roll >> 24) as usize] += 1;
+            if (roll >> 29) == 0 {
+                print!("\n\n======================================\n>>>>>>>>>>>>>>>>>>>\n\n");
             }
+            print!("{}", tmp as char);
             */
+
+            // A dictionary rather than an array of size 256 destroys performance
+            // 14 seconds versus ... indefinitely long ...
+            //if counts.contains_key(tmp) {
+            //    *counts.get_mut(tmp).unwrap() += 1;
+            //} else {
+            //    counts.insert(tmp.to_owned(), 1);
+            //}
         }
     }
     println!("Seen: {}", seen);
