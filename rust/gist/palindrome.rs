ae45b8262a73061f1f8409daf42b977e996ef905
Stephen Merity <smerity@smerity.com>
2019-02-16 14:41:10 -0800

    Make generic with T and note that it needs the PartialEq trait

--- rust/gist/palindrome.rs
+++ rust/gist/palindrome.rs
@@ -1,23 +1,21 @@
-use std::env;
-use std::fs;
-
 // Use double ended iterator:
 // https://doc.rust-lang.org/std/iter/trait.DoubleEndedIterator.html
 //
 // Inspired by:
 // https://www.reddit.com/r/rust/comments/7a9w1c/rust_is_now_an_official_part_of_stanfords/dp8rx8j/
 
-fn main() {
-    let mut s = "radar".as_bytes();
-
+fn is_palindrome<T: PartialEq>(s: &[T]) -> bool {
     let mut iter = s.iter();
-    let mut palindrome = true;
     while let (Some(a), Some(b)) = (iter.next(), iter.next_back()) {
-        println!("{} {}", *a, *b);
         if *a != *b {
-            palindrome = false;
-            break;
+            return false;
         }
     }
-    println!("Is {:?} a palindrome? {}", s, palindrome);
+    return true;
+}
+
+fn main() {
+    let s = "radar".as_bytes();
+
+    println!("Is {:?} a palindrome? {}", s, is_palindrome(s));
 }
